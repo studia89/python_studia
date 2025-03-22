ef99d0672e3f930e9a2f11d5e9df2ebd8cef773d
Agnieszka Zuber <agnieszkazuber89@gmail.com>
2025-03-22 11:30:09 +0100

    sortowanie przez wybieranie i wstawianie

---
+++ sortowanie/babelkowanie.py
@@ -0,0 +1,8 @@
+dane = [1, 6, 5, 2, 8]
+n = len(dane)
+for i in range (0,n-1):
+    for j in range(0, n - 1 - i):
+        if dane[j] > dane[j + 1]:
+            dane[j], dane[j + 1] = dane[j + 1], dane[j]
+
+print(dane)
