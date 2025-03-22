71a9f47779a6b1162bfe6a3b7cc238554d83e946
Agnieszka Zuber <agnieszkazuber89@gmail.com>
2025-03-22 13:04:06 +0100

    liczby pierwsze

---
+++ liczby_pierwsze/test_pierwszosci.py
@@ -0,0 +1,12 @@
+def czy_pierwsza(liczba : int):
+
+    for i in range(2, int(liczba ** 0.5)+ 1):
+        if liczba % i == 0:
+            return False
+    return True
+
+if __name__ == '__main__':
+    liczba = int(input())
+    wynik = czy_pierwsza(liczba)
+    print(wynik)
+
