c253822d9a9ee6fcc5822c3bbbcc707c8d4b1217
Agnieszka Zuber <agnieszkazuber89@gmail.com>
2025-03-22 13:25:13 +0100

    liczby pierwsze z pierwiastkami podzielnosc 2 i 3

--- liczby_pierwsze/test pierwszosci.py
+++ liczby_pierwsze/test pierwszosci.py
@@ -1,9 +1,14 @@
 def czy_pierwsza(liczba : int):
+    if liczba < 2:
+        return False
 
-    for i in range(2, int(liczba ** 0.5)+ 1):
-        if liczba % i == 0:
+    if liczba % 2 == 0 or liczba % 3 == 0:
+            return False
+    for i in range(6, int(liczba ** 0.5) + 1,6):
+        if liczba % i-1 == 0 or liczba % i+1 == 0:
             return False
     return True
+
 
 if __name__ == '__main__':
     liczba = int(input())
