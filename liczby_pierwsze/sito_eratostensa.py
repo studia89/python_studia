520d18fd4c77dfde6f0f0d40f364c401bcc4a67a
Agnieszka Zuber <agnieszkazuber89@gmail.com>
2025-03-22 14:45:46 +0100

    sito eratostensa 2

--- liczby_pierwsze/sito_eratostensa.py
+++ liczby_pierwsze/sito_eratostensa.py
@@ -1,15 +1,18 @@
 def generuj_liczby_pierwsze(n):
-
     dane = [True] * (n+1)
     dane[0] = dane[1] = False
-    for i in range (2, n+1):
+
+
+    for i in range (2, int(n**0.5)*1):
         if dane [i] == True:
-            for j in range (2*i, n+1, i):
+            for j in range (i**2, n+1, i):
                 dane[j] = False
+
+    wynik = []
     for i in range (n+1):
         if dane[i] == True:
-            print(i)
-
-
+            #print(i)
+            wynik += [i]
+    return wynik
 if __name__ == '__main__':
     generuj_liczby_pierwsze(120)
