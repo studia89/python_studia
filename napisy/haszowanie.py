c5deb64a2845bbc8a3d8a0f0b42e94a192eda930
Agnieszka Zuber <agnieszkazuber89@gmail.com>
2025-04-05 15:25:53 +0200

    haszowanie

---
+++ napisy/haszowanie.py
@@ -0,0 +1,24 @@
+def wyszukaj_przez_haszowanie(sprawdzamy, szukamy):
+    suma = 0
+    suma_szukanego = 0
+
+    for i in range (len(szukamy)):
+        suma += ord(sprawdzamy[i])
+        suma_szukanego += ord (szukamy[i])
+    for i in range(len(sprawdzamy) - len(szukamy)+1):
+        if suma == suma_szukanego:
+            znaleziono = True
+            for j in range(len(szukamy)):
+                if not sprawdzamy[i+j] == szukamy[j]:
+                    znaleziono = False
+                    break
+            if znaleziono:
+                return i
+        suma -= ord(sprawdzamy[i])
+        suma += ord (sprawdzamy [i+len(szukamy)])
+
+
+if __name__ == '__main__':
+    sprawdzamy = "ala ma koguta i kota"
+    szukamy = "ko"
+    print(wyszukaj_przez_haszowanie(sprawdzamy, szukamy))
