ed08118b0e2a37c3611c9dc2d68886417f7b0a26
Agnieszka Zuber <agnieszkazuber89@gmail.com>
2025-04-05 14:42:42 +0200

    wyszukiwanie naiwne2

--- napisy/wyszukiwanie.py
+++ napisy/wyszukiwanie.py
@@ -1,12 +1,19 @@
 def wyszukaj_naiwnie(sprawdzamy, szukamy):
     for i in range(len(sprawdzamy) - len(szukamy) + 1):
-        if sprawdzamy[i:i + len(szukamy)] == szukamy:
+        znaleziono = True
+        for j in range (len(szukamy)):
+            if not sprawdzamy[i+j] == szukamy[j]:
+                znaleziono = False
+                break
+        if znaleziono:
             return i
     return None
+
 
 if __name__ == '__main__':
     sprawdzamy = "ala ma kota"
     szukamy = "ko"
     print(wyszukaj_naiwnie(sprawdzamy, szukamy))
+
 
 
