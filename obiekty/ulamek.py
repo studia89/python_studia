7d5165978cbfad7ffa1de2ce151511407108b3bb
studia89 <agnieszkazuber89@gmail.com>
2025-11-15 15:45:40 +0100

    ulamek

--- obiekty/ulamek.py
+++ obiekty/ulamek.py
@@ -1,44 +1,25 @@
+from math import gcd
+
 class Ulamek:
     def __init__(self, licznik, mianownik):
         self.licznik = licznik
         self.mianownik = mianownik
-
-if __name__ == '__main__':
-    u = Ulamek(3, 4)
-    print(u.licznik, u.mianownik)
-
-self.licznik = licznik
-        self.mianownik = mianownik
+        self.__zredukuj()
 
     def czy_calkowita(self):
         return self.licznik % self.mianownik == 0
 
-if __name__ == '__main__':
-    u = Ulamek(3, 4)
-    print(u.licznik, u.mianownik)
-    u = Ulamek(8, 4)
-    print(u.czy_calkowita())
-
-
- def czy_calkowita(self):
-        return self.licznik % self.mianownik == 0
-
- def __str__(self):
+    def __str__(self):
         return f"{self.licznik}/{self.mianownik}"
 
-def porownaj(a: Ulamek, b: Ulamek):
-    wartosc_a = a.licznik / a.mianownik
-    wartosc_b = b.licznik / b.mianownik
-    if wartosc_a < wartosc_b: return -1
-    elif wartosc_a == wartosc_b: return 0
-    else: return 1
+    def __float__(self):
+        return self.licznik / self.mianownik
 
-if __name__ == '__main__':
-    u = Ulamek(8, 4)
-    print(u.czy_calkowita())
-    print(porownaj(Ulamek(3,  4), Ulamek(2, 3)))
-
-    print(u.__str__())
+    def zredukuj(self):
+    def __zredukuj(self):
+        nwd = gcd(self.licznik, self.mianownik)
+        self.licznik //= nwd
+        self.mianownik //= nwd
 
 def porownaj(a: Ulamek, b: Ulamek):
     wartosc_a = float(a)
@@ -47,15 +28,32 @@ def porownaj(a: Ulamek, b: Ulamek):
     elif wartosc_a == wartosc_b: return 0
     else: return 1
 
- def __float__(self):
-        return self.licznik / self.mianownik
+if __name__ == '__main__':
+    u = Ulamek(12, 36)
+    u.zredukuj()
+    u = Ulamek(12, 100)
+    print(u)
 
-    def zredukuj(self):
-        nwd = gcd(self.licznik, self.mianownik)
+    #print(float(u))
+
         self.licznik //= nwd
         self.mianownik //= nwd
 
+    def pomnoz(self, inny):
+        return Ulamek(
+            self.licznik * inny.licznik,
+            self.mianownik * inny.mianownik
+        )
 
-u = Ulamek(12, 36)
-u.zredukuj()
-print(u)
+def porownaj(a: Ulamek, b: Ulamek):
+    wartosc_a = float(a)
+    wartosc_b = float(b)
+@@ -28,8 +34,9 @@ def porownaj(a: Ulamek, b: Ulamek):
+    else: return 1
+
+if __name__ == '__main__':
+    u = Ulamek(12, 100)
+    print(u)
+    u1 = Ulamek(2, 3)
+    u2 = Ulamek(3, 4)
+    print(u1.pomnoz(u2))
